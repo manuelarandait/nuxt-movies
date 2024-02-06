d317fc401e4bc38f02ce07fc06c7bce1dd84f214
manuelarandait <157364247+manuelarandait@users.noreply.github.com>
2024-02-06 12:55:49 +0100

    Added meta tags

--- app.vue
+++ app.vue
@@ -1,3 +1,28 @@
+<script setup>
+useHead({
+  htmlAttrs: {
+    lang: 'es',
+  },
+  charset: 'utf-8',
+  title: 'Nuxt Movies',
+  titleTemplate: title => title !== 'Nuxt Movies' ? `${title} · Nuxt Movies` : title,
+  meta: [
+    { name: 'description', content: 'A TMDB client built with Nuxt' },
+    { property: 'og:image', content: 'https://movies.nuxt.space/social-card.png' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:site', content: '@manuelaranda9' },
+    { name: 'twitter:creator', content: '@manuelaranda9' },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/webp',
+      href: '/movies.webp',
+    },
+  ],
+})
+</script>
+
 <template>
   <v-app>
     <NuxtLoadingIndicator />
@@ -10,7 +35,7 @@
 <style>
 .page-enter-active,
 .page-leave-active {
-  transition: all 0.4s;
+  transition: all 0.2s;
 }
 .page-enter-from,
 .page-leave-to {
