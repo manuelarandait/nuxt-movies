630431ca7a35290104de6ed33d8771f9b5fc325a
manuelarandait <157364247+manuelarandait@users.noreply.github.com>
2024-02-12 23:12:28 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
-    ssr: false,
     css: ['~/assets/css/main.css'],
     build: {
         transpile: ['vuetify']
